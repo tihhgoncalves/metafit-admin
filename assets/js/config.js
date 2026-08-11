08596fba91ffde0db26bdc34f0cc799a9832ec91
Tihh Gonçalves <tihhgoncalves@gmail.com>
2026-08-11 20:29:57 -0300

    Ajustando caminho do config

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 (() => {
   const request = new XMLHttpRequest();
-  request.open('GET', new URL('../../config.php', document.currentScript.src), false);
+  request.open('GET', new URL('../../config/config.php', document.currentScript.src), false);
   request.send();
 
   if (request.status !== 200) throw new Error('Não foi possível carregar a configuração do ambiente.');
