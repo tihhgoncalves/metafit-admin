a18b95a8b725337fafea1ae630cc9a08a93cd5d8
Tihh Gonçalves <tihhgoncalves@gmail.com>
2026-08-11 20:18:06 -0300

    Squashed commit of the following:
    commit 88b82023315f20858276d2737479d4ccefdeb165
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Tue Aug 11 20:13:05 2026 -0300
    
        feat: carrega configuração da API por ambiente
    
    commit 62bb203d5af48fd89759a424df73879113c7f311
    Author: Tihh Gonçalves <tihhgoncalves@gmail.com>
    Date:   Tue Aug 11 20:02:57 2026 -0300
    
        chore: redefine versão inicial do projeto

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+header('Content-Type: application/json; charset=utf-8');
+header('Cache-Control: no-store');
+
+$environmentPath = __DIR__ . '/.env';
+if (!is_file($environmentPath)) {
+  http_response_code(500);
+  echo json_encode(['error' => 'Configuração de ambiente ausente.']);
+  exit;
+}
+
+$environment = parse_ini_file($environmentPath, false, INI_SCANNER_RAW);
+$nodeEnvironment = (string) ($environment['NODE_ENV'] ?? 'production');
+$apiUrl = rtrim((string) ($environment['METAFIT_API_URL'] ?? ''), '/');
+if ($apiUrl === '') {
+  http_response_code(500);
+  echo json_encode(['error' => 'METAFIT_API_URL não configurada.']);
+  exit;
+}
+
+echo json_encode(['environment' => $nodeEnvironment, 'apiUrl' => $apiUrl], JSON_UNESCAPED_SLASHES);
